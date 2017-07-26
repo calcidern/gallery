13fc4d0aa69c14be63e402eabd3131b87b53d2e1
TREMORLOR\calcidern-laptop <kadadel@gmail.com>
2017-07-26 13:37:25 +0200

    kitten eyes finished

--- src/components/Kitten.js
+++ src/components/Kitten.js
@@ -5,12 +5,19 @@
 import React from 'react';
 import './Kitten.css';
 
-export const Kitten = () =>{
-  return(
+export const Kitten = () => {
+  return (
     <div className="kitten">
-      <div className="kitten__ear  kitten__ear-right"></div>
-      <div className="kitten__ear  kitten__ear-left"></div>
+      <div className="kitten__ear  kitten__ear-right"/>
+      <div className="kitten__ear  kitten__ear-left"/>
 
+      <div className="kitten__eyes">
+
+        <div className="kitten__eye kitten__eye--left">
+        </div>
+        <div className="kitten__eye kitten__eye--right">
+        </div>
+      </div>
 
     </div>
   )
