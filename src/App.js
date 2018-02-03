055e8659daa5045b9925a3e7299f6a8f0e8dcbc4
TREMORLOR\calcidern-laptop <kadadel@gmail.com>
2018-02-03 19:05:22 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
-// import {Velocirator} from './components/Velociraptor';
-import {TigerCub} from './components/TigerCub';
+import {Switch, Route} from "react-router-dom";
+import {TigerCub} from "./components/TigerCub";
 import {Morty} from './components/Morty';
-// import {Kitten} from './components/Kitten';
-// import {Flower} from './components/Flower';
 import {Bird} from './components/Bird';
 import {Ghost} from './components/Ghost';
 import TwinSun from './components/TwinSun';
@@ -14,17 +12,22 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const components=[
+      <TigerCub/>,
+      <Morty/>,
+      <Bird/>,
+      <Ghost/>,
+      <BeforeCoffee/>
+    ];
+
     return (
       <div className="App">
-        {/*<TwinSun/>*/}
-        <TigerCub/>
-        <Morty/>
-        <Bird/>
-        <Ghost/>
-        <BeforeCoffee/>
-        {/*<Velocirator/>*/}
-        {/*<Kitten/>*/}
-        {/*<Flower/>*/}
+        <Switch>
+          <Route exact path="/" component={()=>components}/>
+          <Route path="/:id" component={({match})=>components[match.params.id]||<div>Not yet</div>}/>
+          <Route component={<div>Nope</div>}/>
+
+        </Switch>
       </div>
     );
   }
